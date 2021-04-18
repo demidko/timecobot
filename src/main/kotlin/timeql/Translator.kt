3528892ee6f27e23c1ccf246dfcfe5fa67368506
Daniil Demidko <demidko@farpost.com>
2021-04-18 10:04:36 +1000

    wip

--- src/main/kotlin/timeql/Translator.kt
+++ src/main/kotlin/timeql/Translator.kt
@@ -5,6 +5,6 @@ package timeql
  * Переводим набор токенов контекстно свободной грамматики `забанить на 5 минут`
  * в промежуточное представление `[command] [number] [time unit]`
  */
-fun List<String>.translate(): Command {
+private fun List<String>.translate(): Command {
   TODO("WIP")
 }
