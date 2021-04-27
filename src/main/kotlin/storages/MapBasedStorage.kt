2ef77cd844a13ecbf9f14521d179a32267baa077
Daniil Demidko <demidko@farpost.com>
2021-04-27 21:20:04 +1000

    тестирую Redis

--- src/main/kotlin/storages/MapBasedStorage.kt
+++ src/main/kotlin/storages/MapBasedStorage.kt
@@ -1,21 +1,7 @@
 package storages
 
-import org.redisson.Redisson
-import org.redisson.config.Config
-import org.slf4j.LoggerFactory.getLogger
 import kotlin.time.Duration
 
 fun mapBasedStorage(): MutableMap<Long, Duration> {
-  return try {
-    Config()
-      .apply {
-        useClusterServers()
-          .addNodeAddress(System.getenv("DATABASE_URL"))
-      }
-      .let(Redisson::create)
-      .getMap("timecoins")
-  } catch (e: RuntimeException) {
-    getLogger("Redis").warn(e.message, e)
-    LinkedHashMap()
-  }
+  return LinkedHashMap()
 }
---
+++ src/test/kotlin/storages/MapBasedStorageKtTest.kt
@@ -0,0 +1,34 @@
+package storages
+
+import org.junit.jupiter.api.Test
+import org.redisson.Redisson.create
+import org.redisson.config.Config
+import java.lang.System.getenv
+import kotlin.streams.toList
+
+internal class MapBasedStorageKtTest {
+
+
+
+  fun mapBasedStorage() {
+    val config = Config().apply {
+      useSingleServer().apply {
+        username = ""
+        password = ""
+        address = ""
+      }
+    }
+    val client = create(config)
+    val map = client.getMap<Long, Long>("test-redis2")
+    map[1] = 4
+    println(client.keys.keysStream.toList())
+  }
+
+  @Test
+  fun testRedis() {
+    println(getenv("REDIS_URL"))
+    println(getenv("REDIS_USER"))
+    println(getenv("REDIS_PASSWORD"))
+    println(getenv("DATABASE_URL"))
+  }
+}
