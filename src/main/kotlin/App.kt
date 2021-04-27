b3f1cfaf9af487763ba00a7526e0124184294989
Daniil Demidko <demidko@farpost.com>
2021-04-28 01:59:35 +1000

    redis IT 2

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -7,8 +7,13 @@ fun main(args: Array<String>) {
 
   println(System.getenv("REDIS_URL"))
   println(System.getenv("REDIS_USER"))
+  println(System.getenv("REDIS_USERNAME"))
   println(System.getenv("REDIS_PASSWORD"))
+
   println(System.getenv("DATABASE_URL"))
+  println(System.getenv("DATABASE_USER"))
+  println(System.getenv("DATABASE_USERNAME"))
+  println(System.getenv("DATABASE_PASSWORD"))
 
   val bank = TimeBank()
   val log = getLogger("Bot")
