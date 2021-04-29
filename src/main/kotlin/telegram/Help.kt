90b4c9fa31546e5aea933ebaf26a23ea130ed986
Daniil Demidko <demidko@farpost.com>
2021-04-30 04:39:58 +1000

    FAQ

--- src/main/kotlin/telegram/Help.kt
+++ src/main/kotlin/telegram/Help.kt
@@ -9,6 +9,7 @@ const val faqRu = """
 
 1. Время (валюта) начисляется вам автоматически, каждую минуту, совершенно бесплатно и безусловно.
    Чтобы узнать свой баланс, напишите в чат запрос, например слово баланс, статус или знак !
+   (В ответе используются сокращения d - дни, h - часы, m - минуты, s - секунды)
    
 2. Чтобы забанить человека используйте приказы вида `бан 5 минут` или `блок на 2 часа` в ответном сообщении человеку.
    Я заблокирую его на указанное вами время: он останется в чате, но не сможет ничего писать.
@@ -23,6 +24,7 @@ const val faqRu = """
 const val faqEn = """
 1. Time (currency) is credited to you automatically, every minute, completely free of charge and unconditionally.
    To check your balance, send a request to the chat, for example word balance, status or ! symbol.
+   (In answer, d - days, h - hours, m - minutes, s - seconds)
    
 2. To ban a person, use orders like `ban 5 minutes` or `block for 2 hours` in the reply message to that person.
    I will block him for the time you specified: he will remain in the chat, but he will not be able to write anything.
