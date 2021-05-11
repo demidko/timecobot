3cc48fc5259167ec403188d61c76afc33a39a2db
Daniil Demidko <demidko@farpost.com>
2021-05-11 20:44:32 +1000

    elapsed timelog

--- src/main/kotlin/Router.kt
+++ src/main/kotlin/Router.kt
@@ -1,13 +1,23 @@
 import com.github.kotlintelegrambot.dispatcher.handlers.TextHandlerEnvironment
+import org.slf4j.LoggerFactory.getLogger
 import speech.*
 import telegram.*
+import kotlin.system.measureTimeMillis
+import kotlin.time.Duration.Companion.milliseconds
+
+private val log = getLogger("Router")
 
 fun TextHandlerEnvironment.routeRequest() {
-  when (val command = text.parseCommand()) {
-    is BanCommand -> bot.ban(command.duration, message)
-    is FreeCommand -> bot.free(message)
-    is StatusCommand -> bot.status(message)
-    is TransferCommand -> bot.transfer(command.duration, message)
-    is HelpCommand -> bot.help(message)
+  val elapsedTime = measureTimeMillis {
+    when (val command = text.parseCommand()) {
+      is BanCommand -> bot.ban(command.duration, message)
+      is FreeCommand -> bot.free(message)
+      is StatusCommand -> bot.status(message)
+      is TransferCommand -> bot.transfer(command.duration, message)
+      is HelpCommand -> bot.help(message)
+    }
+  }
+  if (elapsedTime > 500) {
+    log.warn("Too large request processed ${milliseconds(elapsedTime)}: $text")
   }
 }
