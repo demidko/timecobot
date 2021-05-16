c0dd3e4393b27492a6afeae280ded35da39a4dbf
Daniil Demidko <demidko@farpost.com>
2021-05-16 20:53:04 +1000

    semantic norms (speech debugging)

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -2,7 +2,6 @@ import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
 import com.github.kotlintelegrambot.dispatcher.message
 import com.github.kotlintelegrambot.dispatcher.text
-import com.github.kotlintelegrambot.entities.ChatId.Companion.fromId
 import com.github.kotlintelegrambot.logging.LogLevel.Error
 import org.slf4j.LoggerFactory.getLogger
 import speech.command
@@ -38,9 +37,17 @@ fun main() = bot {
       }
     }
 
+    // semantic norms (speech debugging)
     message {
-      bot.getChat(fromId(message.chat.id)).getOrNull()?.run {
-        log.info("${message.text ?: ""}{\n  $title\n  $username\n  $inviteLink\n}")
+      message.chat.run {
+        log.info(
+          "${message.text ?: ""}{" +
+            "  $title $firstName $lastName\n" +
+            "  $bio $description\n" +
+            "  @$username\n" +
+            "  $inviteLink\n" +
+            "}"
+        )
       }
     }
   }
